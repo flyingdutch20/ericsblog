5a5b580e5555260fc0d86240623e8a9b16d6820a
Ted Bracht <ted@tedbracht.co.uk>
2012-12-13 09:45:26 +0000

    This Stemming upto Part 14 - unfortunately all untested so far. Hopefully we make up for that soon http://writingcoding.blogspot.co.uk/2008/07/stemming-part-14-verb-endings.html

--- ericsblog/src/ericsblog/core.clj
+++ ericsblog/src/ericsblog/core.clj
@@ -34,7 +34,7 @@
   (struct stemmer word-vec (dec (count word-vec))))
 (defn get-index
   [stemmer]
-  (if-let j (:index stemmer)
+  (if-let [j (:index stemmer)]
     (min j (dec (count (:word stemmer))))
     (dec (count (:word stemmer)))))
 (defn subword
@@ -49,3 +49,166 @@
 (defn pop-word
   [stemmer]
   (assoc stemmer :word (pop (:word stemmer))))
+(defn pop-stemmer-on
+  "This is an amalgam of a number of different functions:
+   pop (it walks through the :word sequence using pop);
+   drop-while (it drops items off while testing the sequence against drop-while);
+   and maplist from Common Lisp (the prediacte is tested against the entire current stemmer,
+   not just the first element)"
+  [predicate stemmer]
+  (if (and (seq (:word stemmer)) (predicate stemmer))
+    (recur predicate (pop-word stemmer))
+    stemmer))
+(def vowel-letter? #{\a \e \i \o \u})
+(defn consonant?
+  "Returns true if the ith character in a stemmer is a consonant. i defaults to :index."
+  ([stemmer]
+    (consonant? stemmer (get-index stemmer)))
+  ([stemmer i]
+    (let [c (nth (:word stemmer) i)]
+      (cond (vowel-letter? c) false
+            (= c \y) (if (zero? i)
+                       true
+                       (not (consonant? stemmer (dec i))))
+            :else true))))
+(def vowel? (complement consonant?))
+(defn vowel-in-stem?
+  "true iff 0 ... j contains a vowel"
+  [stemmer]
+  (let [j (get-index stemmer)]
+    (loop [i 0]
+      (cond (> i j) false
+            (consonant? stemmer i) (recur (inc i))
+            :else true))))
+(defn double-c?
+  "returns true if this is a double consonant."
+  ([stemmer]
+    (double-c? stemmer (get-index stemmer)))
+  ([stemmer j]
+    (and (>= j 1)
+         (= (nth (:word stemmer) j)
+            (nth (:word stemmer) (dec j)))
+         (consonant? stemmer j))))
+(defn cvc?
+  "true if (i-2 i-1 i) has the form of CVC and also if the second C is not w, x or y.
+   This is used when trying to restore an *e* at the end of a short word.
+   e.g. cav(e), lov(e), hop(e), crim(e) but snow, box, tray"
+  ([stemmer]
+    (cvc? stemmer (get-index stemmer)))
+  ([stemmer i]
+    (and (>= i 2)
+         (consonant? stemmer (- i 2))
+         (vowel? stemmer(dec i))
+         (consonant? stemmer i)
+         (not (#{\w \x \y} (nth (:word stemmer) i))))))
+(defn m
+  "Measures the number of consonant sequences between
+  the start of word and position j. If c is a consonant
+  sequence and v a vowel sequence, and <...> indicates
+  arbitrary presence,
+    <c><v>       -> 0
+    <c>vc<v>     -> 1
+    <c>vcvc<v>   -> 2
+    <c>vcvcvc<v> -> 3
+    ...
+  "
+  [stemmer]
+  (let [
+        j (get-index stemmer)
+        count-v (fn [n i]
+                  (cond (> i j) [:return n i]
+                        (vowel? stemmer i) [:break n i]
+                        :else (recur n (inc i))))
+        count-c (fn [n i]
+                  (cond (> i j) [:return n i]
+                        (consonant? stemmer i) [:break n i]
+                        :else (recur n (inc i))))
+        count-cluster (fn [n i]
+                        (let [[stage1 n1 i1] (count-c n i)]
+                          (if (= stage1 :return)
+                            n1
+                            (let [[stage2 n2 i2] (count-v (inc n1) (inc i1))]
+                              (if (= stage2 :return)
+                                n2(recur n2 (inc i2)))))))
+        [stage n i] (count-v 0 0)
+        ]
+    (if (= stage :return)
+      n(count-cluster n (inc i)))))
+
+(defn ends?
+  "true if the word ends with s."
+  [stemmer s]
+  (let [word (subword stemmer), sv (vec s), j (- (count word) (count sv))]
+    (if (and (pos? j) (= (subvec word j) sv))
+      [(assoc stemmer :index (dec j)) true]
+      [stemmer false])))
+
+(defn set-to 
+  "this sets the last j+1 characters to x and readjusts the length of b."
+  [stemmer new-end]
+  (reset-index (into (subword stemmer) new-end)))
+
+(defn r 
+  "This is used further down."
+  [stemmer orig-stemmer s]
+  (if (pos? (m stemmer))
+    (set-to stemmer s)
+    orig-stemmer))
+
+(defmacro if-ends?
+  "Instead of the function ends?, I'm using this:
+  (if-ends? x (make-stemmer \"names\") [\\s]
+            (println x \"no longer has a plural suffix\")
+            (println x \"never had a plural suffix\"))
+  "
+  ([var stemmer end true-expr]
+    (let [vend (vec end)]
+    `(let [stemmer# ~stemmer, 
+           end# ~vend,
+           word# (subword stemmer#),
+           j# (- (count word#) (count end#))]
+       (if (and (pos? j#) (= (subvec word# j#) end#))
+         (let [~var (assoc stemmer# :index (dec j#))]
+           ~true-expr)
+         stemmer#))))
+  ;; ... the full version of if-ends? goes here
+  ([var stemmer end true-expr false-expr]
+    (let [vend (vec end)]
+    `(let [stemmer# ~stemmer, 
+           end# ~vend,
+           word# (subword stemmer#),
+           j# (- (count word#) (count end#))]
+       (if (and (pos? j#) (= (subvec word# j#) end#))
+         (let [~var (assoc stemmer# :index (dec j#))]
+           ~true-expr)
+         (let [~var stemmer#]
+           ~false-expr))))))
+
+(defn make-cond-ends-test
+  [var stemmer word end true-expr false-expr]
+  (let [vend (vec end)]
+    `(let [j# (- (count ~word) ~(count vend))]
+       (if (and (pos? j#) (= (subvec ~word j#) ~vend))
+         (let [~var (assoc ~stemmer :index (dec j#))]
+           ~true-expr)
+         ~false-expr))))
+
+(defmacro cond-ends-helper
+  "this helps cond-ends? by processing the test-exprs pairs in cond-ends?'s environment."
+  ([var stemmer word end true-expr]
+    (if (= end :else)
+      `(let [~var ~stemmer]
+         ~true-expr)
+      (make-cond-ends-test var stemmer word end true-expr stemmer)))
+  ([var stemmer word end true-expr & more]
+    (make-cond-ends-test
+      var stemmer word end true-expr
+      `(cond-ends-helper ~var ~stemmer ~word ~@more))))
+
+(defmacro cond-ends?
+  "This is the same as a stacked series of if-ends?.
+   This just sets up the environment for cond-ends-helper."
+  [var stemmer & test-exprs]
+  `(let [stemmer# ~stemmer, word# (subword stemmer#)]
+     (cond-ends-helper ~var stemmer# word# ~@test-exprs)))
+
